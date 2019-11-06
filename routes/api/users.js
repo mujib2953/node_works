ea52b3ec14cd36e7089f18dc0b22367774b24c0b
Mujibur Rehman <mujibur.rehman@citiustech.com>
2019-11-06 20:16:14 +0530

    User validation added.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
+
+// --- Users model
+const User = require("../../models/User");
 
 /*
 * @route_type   : GET
@@ -17,9 +21,41 @@ router.get("/", (req, res) => {
 * @desc         : with this new User can register themself with application
 * @access       : public
 */
-router.post("/register", (req, res) => {
-    console.log(req.body);
-    res.send("Registered new User successfully.");
+router.post("/register", [
+    check("name", "Name is required.").not().isEmpty(),
+
+    check("email", "Please include a valid email.").isEmail(),
+
+    check("password", "Please enter password with 6 or more charcters").isLength({ min: 6 })
+],
+async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        // --- check user is already exists
+        let user = await User.findOne({ email });
+
+        if (user) {
+            res.status(400).json({ errors: [{ message: "User already exists." }] });
+        }
+
+        // --- get users gravatar
+
+        // --- encrypt users password
+
+        // --- return jwt
+
+        res.send("Registered new User successfully.");
+    } catch(e) {
+        console.error(e.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 module.exports = router;
