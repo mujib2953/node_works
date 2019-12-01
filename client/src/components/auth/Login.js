83a4fb1fcd4d756430edd1814066b056cdff6fff
mujib2953 <ansarimujiburrehman@gmail.com>
2019-12-01 19:58:10 +0530

    login and register page event handling added.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+    const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log("SUCCESS");
+    }
+
     return (
-        <section className="container">
+        <Fragment>
             <div className="alert alert-danger">
                 Invalid Credentials.
             </div>
@@ -13,21 +27,38 @@ export default function Login() {
                 Sign into Your Account
             </p>
 
-            <form action="dashboard.html" className="form">
+            <form
+                className="form"
+                onSubmit={ e => onSubmit(e) }
+            >
                 <div className="form-group">
-                    <input type="email" name="email" placeholder="Email Address" required />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        value={ email }
+                        onChange={ e => handleChange(e) }
+                        required
+                    />
                 </div>
 
                 <div className="form-group">
-                    <input type="password" name="password" placeholder="Password" required />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={ password }
+                        onChange={ e => handleChange(e) }
+                        required
+                    />
                 </div>
 
                 <input type="submit" className="btn btn-primary" value="Sign In" />
             </form>
 
             <p className="my-1">
-                Don't have an account? <a href="register.html">Sign Up</a>
+                Don't have an account? <Link to="/register">Sign Up</Link>
             </p>
-        </section>
+        </Fragment>
     );
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,39 +1,90 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Register() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+
+    const { name, email, password, password2 } = formData;
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        if (password !== password2) {
+            console.error("Passwords does not match.");
+        } else {
+            console.log("SUCCESS");
+        }
+    }
     return (
-        <section className="container">
+        <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead">
                 <i className="fas fa-user"></i> Create Your Account
             </p>
 
-            <form className="form" action="create-profile.html">
+            <form
+                className="form"
+                onSubmit={ e => onSubmit(e) }
+            >
                 <div className="form-group">
-                    <input type="text" name="name" placeholder="Name" required />
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={ name }
+                        onChange={ e => onChange(e) }
+                        required
+                    />
                 </div>
 
                 <div className="form-group">
-                    <input type="email" name="email" placeholder="Email Address" required />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        value={ email }
+                        onChange={ e => onChange(e) }
+                        required
+                    />
                     <small className="form-text">
                         This site uses Gravatar so if you want profile image, use a Gravatar email.
                     </small>
                 </div>
 
                 <div className="form-group">
-                    <input type="password" name="password" placeholder="Password" minlength="6" />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={ password }
+                        onChange={ e => onChange(e) }
+                        minLength="6"
+                    />
                 </div>
 
                 <div className="form-group">
-                    <input type="password" name="password2" placeholder="Confirm Password" minlength="6" />
+                    <input
+                        type="password"
+                        name="password2"
+                        placeholder="Confirm Password"
+                        value={ password2 }
+                        onChange={ e => onChange(e) }
+                        minLength="6"
+                    />
                 </div>
 
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
 
             <p className="my-1">
-                Already have an accont? <a href="login.html">Sign In</a>
+                Already have an accont? <Link to="/login">Sign In</Link>
             </p>
-        </section>
+        </Fragment>
     );
 }
