36e738f155b0d4462b71e6c2bbabf8f6d988520d
mujib2953 <ansarimujiburrehman@gmail.com>
2019-12-01 20:28:12 +0530

    setup redux done.

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,29 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
+// redux related
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./css/Common.css";
 import "./css/App.css";
 
 function App() {
 	return (
-		<Router>
-			<Fragment>
-				<Navbar />
-				<Route exact path="/" component={ Landing } />
-				<section className="container">
-					<Switch>
-						<Route exact path="/login" component={ Login } />
-						<Route exact path="/register" component={ Register } />
-					</Switch>
-				</section>
-			</Fragment>
-		</Router>
+		<Provider store={ store }>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Route exact path="/" component={ Landing } />
+					<section className="container">
+						<Switch>
+							<Route exact path="/login" component={ Login } />
+							<Route exact path="/register" component={ Register } />
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
 	);
 }
 
