56e92e6ef94c04606f1ad2ec7e7879a78f6dae4c
mujib2953 <ansarimujiburrehman@gmail.com>
2019-12-07 13:00:46 +0530

    user profile completed.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,11 @@ import { Link } from "react-router-dom";
 import { getProfileById } from "../../actions/profile";
 
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import Education from "../dashboard/Education";
+import ProfileEducation from "./ProfileEducation";
 
 const Profile = ({
     match,
@@ -35,96 +40,49 @@ const Profile = ({
 
             <div className="profile-grid my-1">
                 
-                <div className="profile-top bg-primary p-2">
-                    <img
-                        className="round-img my-1"
-                        src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-                        alt=""
-                    />
-                    <h1 className="large">John Doe</h1>
-                    <p className="lead">Developer at Microsoft</p>
-                    <p>Seatle, WA</p>
+                <ProfileTop profile={ profile } />
 
-                    <div className="icons my-1">
-                        <a href="#" target="_blank" rel="noopener noreferrer">
-                            <i className="fas fa-globe fa-2x"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-twitter fa-2x"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-facebook fa-2x"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-linkedin fa-2x"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-youtube fa-2x"></i>
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-instagram fa-2x"></i>
-                        </a>
-                    </div>
-                </div>
-
-                <div className="profile-about bg-light p-2">
-                    <h2 className="text-primary">John's Bio</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
-                        doloremque nesciunt, repellendus nostrum deleniti recusandae nobis
-                        neque modi perspiciatis similique?
-                    </p>
-                    <div className="line"></div>
-                    <h2 className="text-primary">Skill Set</h2>
-                    <div className="skills">
-                        <div className="p-1"><i className="fa fa-check"></i> HTML</div>
-                        <div className="p-1"><i className="fa fa-check"></i> CSS</div>
-                        <div className="p-1"><i className="fa fa-check"></i> JavaScrip</div>
-                        <div className="p-1"><i className="fa fa-check"></i> Python</div>
-                        <div className="p-1"><i className="fa fa-check"></i> C#</div>
-                    </div>
-                </div>
+                <ProfileAbout profile={ profile } />
                 
                 <div className="profile-exp bg-white p-2">
                     <h2 className="text-primary">Experience</h2>
-                    <div>
-                        <h3 className="text-dark">Microsoft</h3>
-                        <p>Oct 2011 - Current</p>
-                        <p><strong>Position: </strong>Senior Developer</p>
-                        <p>
-                            <strong>Description: </strong>Lorem ipsum dolor sit amet
-                            consectetur adipisicing elit. Dignissimos placeat, dolorum ullam
-                            ipsam, sapiente suscipit dicta eius velit amet aspernatur
-                            asperiores modi quidem expedita fugit.
-                        </p>
-                    </div>
-                    <div>
-                        <h3 className="text-dark">Sun Microsystems</h3>
-                        <p>Nov 2004 - Nov 2011</p>
-                        <p><strong>Position: </strong>Systems Admin</p>
-                        <p>
-                            <strong>Description: </strong>Lorem ipsum dolor sit amet
-                            consectetur adipisicing elit. Dignissimos placeat, dolorum ullam
-                            ipsam, sapiente suscipit dicta eius velit amet aspernatur
-                            asperiores modi quidem expedita fugit.
-                        </p>
-                    </div>
+
+                    {
+                        profile.experience.length > 0 ? (
+                            <Fragment>
+                                {
+                                    profile.experience.map(experience => (
+                                        <ProfileExperience
+                                            key={experience._id}
+                                            experience={ experience }
+                                        />
+                                    ))
+                                }
+                            </Fragment>
+                        ) : (
+                            <h4>No experience credential.</h4>
+                        )
+                    }
                 </div>
 
                 <div className="profile-edu bg-white p-2">
                     <h2 className="text-primary">Education</h2>
-                    <div>
-                        <h3>University Of Washington</h3>
-                        <p>Sep 1993 - June 1999</p>
-                        <p><strong>Degree: </strong>Masters</p>
-                        <p><strong>Field Of Study: </strong>Computer Science</p>
-                        <p>
-                        <strong>Description: </strong>Lorem ipsum dolor sit amet
-                        consectetur adipisicing elit. Dignissimos placeat, dolorum ullam
-                        ipsam, sapiente suscipit dicta eius velit amet aspernatur
-                        asperiores modi quidem expedita fugit.
-                        </p>
-                    </div>
+                    {
+                        profile.education.length > 0 ? (
+                            <Fragment>
+                                {
+                                    profile.education.map(education => (
+                                        <ProfileEducation
+                                            key={ education._id }
+                                            education={ education }
+                                        />
+                                    ))
+                                }
+                            </Fragment>
+                        ) : (
+                            <h4>No Education Credential.</h4>
+                        )
+                    }
                 </div>
 
                 <div className="profile-github">
