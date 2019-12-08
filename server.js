d78d34ba9497e7360c9d23e453e0afa2ae457803
mujib2953 <ansarimujiburrehman@gmail.com>
2019-12-08 13:05:26 +0530

    Heroku deployment changes.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,15 +12,21 @@ connectDB();
 // --- init middlewares
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.send("API running");
-});
-
 // --- Defining all the routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// --- serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // --- set static folder
+    app.use(express.static("client/build"));
+
+    app.use("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () =>{
     console.log(`Server started on ${PORT} port.`);
